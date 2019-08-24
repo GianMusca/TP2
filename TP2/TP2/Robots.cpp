76daa813817306959693f905358dd0d6748c7981
santiago mangone <santiago_mangone@hotmail.com>
2019-08-24 16:17:53 -0300

    Envio cambios a Robots y Sim -Santi

--- TP2/TP2/Robots.cpp
+++ TP2/TP2/Robots.cpp
@@ -1,3 +1,6 @@
+#include <math.h>
+#include<cstdlib>
+#include<ctime>
 
 typedef struct
 {
@@ -5,7 +8,7 @@ typedef struct
 	double x;
 	double y;
 
-} robotType;
+} robotType;			//Estructura Robots.
 
 robotType* createRobots(uint RobotCount, uint width, uint height); //NULL if error
 void moveRobot(robotType* Robots, uint RobotCount, uint width, uint height);
@@ -14,3 +17,70 @@ void destroyRobots(robotType* robots);
 double getRobotAngle(robotType*);
 double getRobotX(robotType*);
 double getRobotY(robotType*);
+
+double getRobotAngle(robotType* robot)		//Devuelve el angulo de un robot.
+{
+	return (robot->angle);
+}
+
+double getRobotX(robotType* robot)			//Devuelve la componente X de la posicion de un robot.
+{
+	return (robot->x);
+}
+
+double getRobotY(robotType* robot)			//Devuelve la componente Y de la posicion de un robot.
+{
+	return (robot->y);
+}
+
+void destroyRobots(robotType* robots)		//Destruye todos los robots.
+{
+		robots = (robotType*) NULL;
+}
+
+
+robotType* createRobots(uint RobotCount, uint width, uint height)	//Crea todos los robots, asignandole una posicion en el tablero y un angulo.
+{
+	robotType* robots;
+	robots = (robotType*) malloc (width * height * sizeof(robotType));
+	for (int i = 0; i < RobotCount; i++)
+	{
+		*(robots + i)->angle = srand(time(NULL)) % 360;
+		*(robots + i)->y = (srand(time(NULL)) % (height-1))+1;		// asigna a la posicion valores entre 1 y height/width-1 
+		*(robots + i)->x = (srand(time(NULL)) % (width-1))+1;
+	}
+
+	return robots;
+	
+}
+
+void moveRobot(robotType* robots, uint RobotCount, uint width, uint height)		//DISCLAIMER: No esta terminada. Los "calculos" serian esos pero despues 
+																				//faltaria la parte del analisis (es decir, si al moverse toca bordes, etc). Despues lo hago :D
+{
+	for (int i = 0; i < RobotCount; i++)
+	{
+		if (getRobotAngle(robot + i) < 90)
+		{
+			(robots + i)->x += sin((robots + i)->angle);
+			(robots + i)->y += cos((robots + i)->angle);
+		}
+
+		else if (getRobotAngle(robots + i) > 90 && getRobotAngle(robots + i) < 180)
+		{
+			(robots + i)->x += cos((robots + i)->angle - 90.0);
+			(robots + i)->y -= sin((robots + i)->angle - 90.0);
+		}
+
+		else if (getRobotAngle (robots + i) > 180 && getRobotAngle(robots + i) < 270)
+		{
+			(robots + i)->x -= sin((robots + i)->angle - 180.0);
+			(robots + i)->y -= cos((robots + i)->angle - 180.0);
+		}
+
+		else if (getRobotAngle(robots + i) > 270 && getRobotAngle(robots + i) < 360)
+		{
+			(robots + i)->x -= cos((robots + i)->angle - 270.0);
+			(robots + i)->y += sin((robots + i)->angle - 270.0);
+		}
+	}
+}
--- TP2/TP2/Simulation.cpp
+++ TP2/TP2/Simulation.cpp
@@ -33,3 +33,13 @@ bool isSimOver(simulationType* sim)
 {
 	return isFloorClean(sim->floor, sim->width, sim->height);
 }
+
+bool simulationStep(simulationType* sim)
+{
+	moveRobot(sim->robots, sim->robotCount, sim->width, sim->height);		// Mueve todos los robots con un llamado
+	for (int i = 0; i < sim->robotCount; i++)
+	{
+		cleanTile(sim->floor, sim->(robots+i)->x, sim->(robots+i)->y, sim->width);		// Limpia todas las casillas en las que haytan robots.
+																						// para ello se llama multiples veces a la funcion cleanTile.
+	}
+}
